c86cada1cfaa3e3242ff49291832467259a92a23
Ma, Sang-gil <masanggil1986@gmail.com>
2019-03-04 15:29:33 +0900

    meg update

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -217,7 +217,7 @@ Value listallunspent(const Array& params, bool fHelp)
 
     CBitcoinAddress address(params[0].get_str());
     if (!address.IsValid())
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Bitcoin address");
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Meconcash address");
     CTxDestination dest = address.Get();
 
     std::set<CExtDiskTxPos> setpos;
