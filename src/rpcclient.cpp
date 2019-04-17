300053e4f4d0362cd03166e000d100700b533dfd
Sang-gil Ma <sgma@Sang-gilui-iMac.local>
2019-04-17 13:53:54 +0900

    bugfix

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -179,12 +179,12 @@ Array RPCConvertValues(const std::string &strMethod, const std::vector<std::stri
     if (strMethod == "searchrawtransactions"  && n > 1) ConvertTo<boost::int64_t>(params[1]);
     if (strMethod == "searchrawtransactions"  && n > 2) ConvertTo<boost::int64_t>(params[2]);
     if (strMethod == "searchrawtransactions"  && n > 3) ConvertTo<boost::int64_t>(params[3]);
-    if (strMethod == "listallunspent"         && n > 1) ConvertTo<boost::int64_t>(params[3]);
-    if (strMethod == "listallunspent"         && n > 2) ConvertTo<boost::int64_t>(params[3]);
+    if (strMethod == "listallunspent"         && n > 1) ConvertTo<boost::int64_t>(params[1]);
+    if (strMethod == "listallunspent"         && n > 2) ConvertTo<boost::int64_t>(params[2]);
     if (strMethod == "listallunspent"         && n > 3) ConvertTo<boost::int64_t>(params[3]);
-    if (strMethod == "listallunspent"         && n > 4) ConvertTo<boost::int64_t>(params[3]);
-    if (strMethod == "getallbalance"          && n > 1) ConvertTo<boost::int64_t>(params[3]);
-    if (strMethod == "getallbalance"          && n > 2) ConvertTo<boost::int64_t>(params[3]);
+    if (strMethod == "listallunspent"         && n > 4) ConvertTo<boost::int64_t>(params[4]);
+    if (strMethod == "getallbalance"          && n > 1) ConvertTo<boost::int64_t>(params[1]);
+    if (strMethod == "getallbalance"          && n > 2) ConvertTo<boost::int64_t>(params[2]);
     if (strMethod == "gettxout"               && n > 1) ConvertTo<boost::int64_t>(params[1]);
     if (strMethod == "gettxout"               && n > 2) ConvertTo<bool>(params[2]);
     if (strMethod == "lockunspent"            && n > 0) ConvertTo<bool>(params[0]);
